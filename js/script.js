5dd1a91bd17430bb158d3030b7211aa0c71d69f1
DASHRATH RANA <dashrath@DASHRATHs-MacBook-Air.local>
2023-05-03 12:06:22 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,134 @@
+// let age = 15;
+// let hasvotercard = 'yes';
+
+// if (age>=14 && hasvotercard == 'yes'){
+
+//     alert("you can vote!");
+
+// } else if(age>=14 && hasvotercard == 'no'){
+
+//     alert('get your voter id card');
+
+// } else {
+
+//     alert("you can not vote");
+// }
+
+
+// let isloggedin = 1;
+// if(isloggedin == 0){
+//    document.write('login'); 
+// } else{
+//     document.write('logout');   
+// }
+
+// const friend1 = 'Roushan';
+// const friend2 = 'Samdish';
+// const friend3 = 'Dashrath';
+
+// const friends = ['Roushan', 'Samdish', 'Dashrath'];
+// document.write(friends);
+
+// document.write(friends[0]);
+// document.write(friends[1]);
+
+// document.write(friends.length);
+// document.write(friends[friends.length - 1]);
+
+// friends[2] = 'jay';
+// document.write(friends);
+
+// const firstname = 'jones';
+// const jones = [firstname, 'shemedtmann', 2037-1991,
+// 'teacher', friends];
+// document.write(jones);
+
+// const calcAge = function (birthyeah){
+//     return 2037 - birthyeah;
+// }
+
+// const years = [1990, 2015, 2018, 2022];
+// calcAge(years);
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length -1]);
+
+// document.write(age1, age2, age3);
+
+// let user;
+// user = "peter";
+
+// alert(user ?? "guest user");
+
+// let input;
+// input = "m";
+
+// if(input == 1){
+
+//     document.write("continue....");
+
+// } else if(input === "y"){
+
+// document.write("continue.....");
+
+// } else if(input === "yes"){
+//     document.write("continue..."); 
+    
+// } else if(input == 0){
+//     document.write("end.....");
+
+// } else if(input === "n"){
+
+//     document.write("end.....");
+
+// } else{
+//     document.write("wrong input");
+// }
+
+// console.log("5" == 5); // true
+// console.log("5" === 5); // false
+// console.log(undefined == null); // true
+// console.log(undefined === null); // false
+
+// const friend1 = 'peter';
+// const friend2 = 'rojar';
+// const friend3 = 'bini';
+
+// const friends = ['peter', 'rojar', 'bini'];
+
+
+// //FOR LOOP
+// for(let i=0; i<3; i++){
+//     document.write(friends[i],"<br>");
+
+// }
+
+// WHILE LOOP 
+
+// let i=0;
+// while(i<3){
+//     console.log(friends[i],"<br>");
+//     i++;
+// }
+
+// for(let friend of friends){
+//     document.write(friend,"<br>");
+// }
+
+// document.write(friends[0],"<br>"); 
+// document.write(friends[1]);
+
+// let arr = ["I", "go", "home"];
+
+// // delete arr[1];
+
+
+// alert( arr[1] );
+
+
+// alert( arr.length );
+
+// let removed = arr.splice(0, 2);
+
+// alert( removed ); 
